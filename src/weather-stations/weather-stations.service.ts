c64f2d2e8219e5a9a7bc34355c7f25a833518957
Nipun Perera <nipun5perera@gmail.com>
2025-05-19 19:44:38 +1000

    Create weather station service

---
+++ src/weather-stations/weather-stations.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { WeatherStation } from './weather-station.entity';
+
+@Injectable()
+export class WeatherStationsService {
+  constructor(
+    @InjectRepository(WeatherStation)
+    private weatherStationsRepository: Repository<WeatherStation>,
+  ) {}
+
+  findAll(): Promise<WeatherStation[]> {
+    return this.weatherStationsRepository.find();
+  }
+}
